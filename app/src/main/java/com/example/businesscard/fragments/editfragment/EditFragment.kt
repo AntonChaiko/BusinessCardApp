821d0444f2a46cb8a11cd4f87d5e0ccba1b92899
Anton Chaiko <a.chaiko.vironit@vironit.ru>
2021-05-27 20:36:44 +0300

    feat(btnActions)

--- app/src/main/java/com/example/businesscard/fragments/editfragment/EditFragment.kt
+++ app/src/main/java/com/example/businesscard/fragments/editfragment/EditFragment.kt
@@ -1,22 +1,89 @@
 package com.example.businesscard.fragments.editfragment
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.businesscard.R
+import com.example.businesscard.databinding.FragmentEditBinding
 
 
 class EditFragment : Fragment() {
-
+    private lateinit var binding: FragmentEditBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_edit, container, false)
+        when (arguments?.isEmpty) {
+            true -> {
 
+            }
+            false -> arguments?.let {
+                val args = EditFragmentArgs.fromBundle(it)
+                binding.fullNameTextInput.setText(args.fullName)
+                binding.phoneTextInput.setText(args.phone)
+                binding.emailTextInput.setText(args.email)
+
+            }
+        }
+
+        binding.editButton.setOnClickListener {
+            findNavController().navigate(R.id.action_editFragment_to_saveFragment)
+        }
+        binding.callButton.setOnClickListener {
+            val callIntent = Intent(Intent.ACTION_DIAL)
+
+            if (binding.phoneTextInput.text.toString().isEmpty()) {
+                Toast.makeText(
+                    requireContext(),
+                    "Phone field must be not empty",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                callIntent.data = Uri.parse("tel:" + binding.phoneTextInput.text.toString())
+                startActivity(callIntent)
+            }
+        }
+        binding.emailButton.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO)
+            val email = binding.emailTextInput.text.toString()
+            if (email.isEmpty()) {
+                Toast.makeText(
+                    requireContext(),
+                    "Email field must be not empty",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                Toast.makeText(
+                    requireContext(),
+                    "Please enter correct email!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                emailIntent.data = Uri.parse("mailto:")
+                emailIntent.putExtra(
+                    Intent.EXTRA_EMAIL,
+                    Array(1) { email })
+
+                startActivity(emailIntent)
+            }
+        }
+        binding.circleImageView.setOnClickListener {
+
+        }
+        return binding.root
     }
+
 }
--- app/src/main/java/com/example/businesscard/fragments/savefragment/SaveFragment.kt
+++ app/src/main/java/com/example/businesscard/fragments/savefragment/SaveFragment.kt
@@ -5,18 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.businesscard.R
+import com.example.businesscard.databinding.FragmentEditBinding
+import com.example.businesscard.databinding.FragmentSaveBinding
 
 
 class SaveFragment : Fragment() {
-
+    private lateinit var binding: FragmentSaveBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_save, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_save, container,false)
+        binding.toolbarSaveButton.setOnClickListener {
+            val direction = SaveFragmentDirections.actionSaveFragmentToEditFragment(
+                binding.fullNameTextInput.text.toString(),
+                binding.phoneTextInput.text.toString(),
+                binding.emailTextInput.text.toString()
+
+            )
+            it.findNavController().navigate(direction)
+        }
+        return binding.root
     }
 
 }
